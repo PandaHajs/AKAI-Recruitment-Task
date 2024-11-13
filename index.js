bb38874cdc1dd6759f313bd63f33e09e5f6745de
Kacper Skowroński <kacpermariaskowronski@protonmail.com>
2024-11-13 12:10:52 +0100

    Remade code
    Got bored so I remade a lot of the code

--- index.js
+++ index.js
@@ -1,64 +1,53 @@
-//Zadanie należy wykonać w typescript i skompilować później na javascript
-// Zadanie 1: Wybierz niezbędne elementy DOM
-// Przykład: Musisz uzyskać odniesienia do elementów takich jak input pliku, przycisk, img i canvas.
-// Wskazówka: Użyj document.getElementById lub podobnych metod, aby uzyskać elementy po ich ID.
 var imageUploadButton = document.querySelector('#convertGrayscale');
 var imageElement = document.querySelector('#uploadedImage');
 var canvasElement = document.querySelector('#grayscaleImage');
 var inputElement = document.querySelector('input[type="file"]');
-// Zadanie 2: Dodaj nasłuchiwacz zdarzeń dla przesyłania obrazu
-// Kiedy użytkownik wybierze obraz, wyświetl go w elemencie <img>.
-// Wskazówka: Możesz użyć API FileReader, aby odczytać plik jako URL danych.
-if (inputElement) {
-    inputElement.addEventListener('change', loadImage);
+if (inputElement && imageElement) {
+    inputElement.addEventListener('change', function () { loadImage(imageElement, canvasElement); });
 }
-function loadImage(event) {
+function loadImage(imageEl, grayScaled) {
     var file = (inputElement === null || inputElement === void 0 ? void 0 : inputElement.files)[0];
     var reader = new FileReader();
     reader.onload = function (e) {
         var _a;
         if (imageElement) {
-            imageElement.setAttribute('src', (_a = e.target) === null || _a === void 0 ? void 0 : _a.result);
+            imageEl.setAttribute('src', (_a = e.target) === null || _a === void 0 ? void 0 : _a.result);
         }
     };
     reader.readAsDataURL(file);
+    if (grayScaled.toDataURL() !== '') {
+        var ctx = grayScaled.getContext('2d');
+        ctx.clearRect(0, 0, grayScaled.width, grayScaled.height);
+    }
 }
-// Zadanie 3: Dodaj nasłuchiwacz zdarzeń do przycisku „Konwertuj na odcienie szarości”
-// Po kliknięciu, skonwertuj wyświetlany obraz na odcienie szarości i pokaż go w elemencie <canvas>.
-// Wskazówka: Musisz użyć elementu canvas i jego kontekstu (2D) oraz zmodyfikować dane pikseli.
-// Zadanie 4: Narysuj przesłany obraz na canvasie
-// Wskazówka: Użyj drawImage() w kontekście canvasa, aby narysować obraz. Upewnij się, że rozmiar canvasa odpowiada rozmiarowi obrazu.
-// Zadanie 5: Skonwertuj obraz na odcienie szarości poprzez manipulowanie danymi pikseli
-// Wskazówka: Użyj getImageData() do pobrania danych pikseli, zastosuj formułę dla odcieni szarości, a następnie użyj putImageData(), aby zaktualizować canvas.
-// Zadanie opcjonalne: Zastanów się, co się stanie, jeśli nie zostanie przesłany żaden obraz, a przycisk odcieni szarości zostanie kliknięty.
-// Wskazówka: Możesz sprawdzić, czy obraz został przesłany, zanim zastosujesz filtr odcieni szarości.
-if (imageUploadButton) {
-    imageUploadButton.addEventListener('click', turnToGrayScale);
+if (imageUploadButton && canvasElement) {
+    imageUploadButton.addEventListener('click', function () { turnToGrayScale(canvasElement.getContext('2d'), canvasElement); });
 }
-function turnToGrayScale() {
-    var context = canvasElement === null || canvasElement === void 0 ? void 0 : canvasElement.getContext('2d');
+function turnToGrayScale(ctx, canvas) {
     var file = (inputElement === null || inputElement === void 0 ? void 0 : inputElement.files)[0];
-    var reader = new FileReader();
-    reader.onload = function (e) {
-        var _a;
-        var image = new Image();
-        image.src = (_a = e.target) === null || _a === void 0 ? void 0 : _a.result;
-        image.onload = function () {
-            if (canvasElement && context) {
-                canvasElement.width = image.width;
-                canvasElement.height = image.height;
-                context.drawImage(image, 0, 0);
-                var imageData = context.getImageData(0, 0, image.width, image.height);
-                var data = imageData.data;
-                for (var i = 0; i < data.length; i += 4) {
-                    var avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
-                    data[i] = avg;
-                    data[i + 1] = avg;
-                    data[i + 2] = avg;
+    if (file) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            var _a;
+            var image = new Image();
+            image.src = (_a = e.target) === null || _a === void 0 ? void 0 : _a.result;
+            image.onload = function () {
+                if (canvas && ctx) {
+                    canvas.height = image.height;
+                    canvas.width = image.width;
+                    ctx.drawImage(image, 0, 0);
+                    var imageData = ctx.getImageData(0, 0, image.width, image.height);
+                    var data = imageData.data;
+                    for (var i = 0; i < data.length; i += 4) {
+                        var avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+                        data[i] = avg;
+                        data[i + 1] = avg;
+                        data[i + 2] = avg;
+                    }
+                    ctx.putImageData(imageData, 0, 0);
                 }
-                context.putImageData(imageData, 0, 0);
-            }
+            };
         };
-    };
-    reader.readAsDataURL(file);
+        reader.readAsDataURL(file);
+    }
 }
